e8883b8c2137e2b8f3c52d915990311c159d913d
Callmephil <phil19-94@hotmail.fr>
2019-05-17 14:04:27 +0300

    Added File Cheks || Auto-Repo Update => Functionality

--- 00_GitManager/GitManager.js
+++ 00_GitManager/GitManager.js
@@ -1,4 +1,5 @@
 var Git = require("nodegit");
+const fs = require('fs')
 
 const RepositoryList = [
   ["https://github.com/fadel100/Submissions", "Fadel"],
@@ -26,9 +27,95 @@ const RepositoryList = [
   ["https://github.com/BaselBarghouth/Submissions", "Basel"],
   ["https://github.com/Basharfrancis/Submissions", "Bashar"],
   ["https://github.com/BringSteam/Submissions", "Charbel"]
-]
+];
 
-RepositoryList.forEach(elem => {
-  console.log(`Completed : ${elem[1]}`)
-  Git.Clone(elem[0], "../" + elem[1]);
-});
+const exercisesFilePaths = {
+  'cv-styling': ['Exercises/CV-Styling/public/style1/style.css', 'Exercises/CV-Styling/public/style2/style.css'],
+}
+
+var rmDir = function(dir, rmSelf) {
+  var files;
+  rmSelf = (rmSelf === undefined) ? true : rmSelf;
+  dir = dir + "/";
+  try { files = fs.readdirSync(dir); } catch (e) { console.log("!Oops, directory not exist."); return; }
+  if (files.length > 0) {
+      files.forEach(function(x, i) {
+          if (fs.statSync(dir + x).isDirectory()) {
+              rmDir(dir + x);
+          } else {
+              fs.unlinkSync(dir + x);
+          }
+      });
+  }
+  if (rmSelf) {
+      // check if user want to delete the directory ir just the files in this directory
+      fs.rmdirSync(dir);
+  }
+}
+
+const cloneRepos = () => {
+  RepositoryList.forEach(elem => {
+    if (fs.existsSync(`../${elem[1]}`))
+    {
+      rmDir(`../${elem[1]}`);
+      console.log(`Updating : ${elem[1]}...`);
+    }
+    
+    Git.Clone(elem[0], "../" + elem[1]);
+    console.log(`Completed : ${elem[1]}`);
+  });
+};
+
+const checkExerciseByName = (path) =>
+{
+  const badPeeps = [];
+  fs.readdir('../', function(err, items) {
+    for (var i = 0; i < items.length; i++) {
+      if (
+        items[i] === ".git" ||
+        items[i] === ".gitignore" ||
+        items[i] === "00_GitManager" ||
+        items[i] === "README.md"
+      ) continue;
+
+      const fullPath = '../' + items[i] + '/' + path;
+      const studentName = items[i];
+      
+      if (!fs.existsSync(fullPath)) {
+        badPeeps.push(studentName);
+        console.log(`${studentName} => Did not complete the exercise : ${path}`);
+      }
+    }
+    console.log(JSON.stringify(badPeeps));
+  });
+}
+
+const main = () => {
+  if (process.argv.length <= 2)
+    cloneRepos();
+  else {
+    if (process.argv[2] == 'check')
+    {
+      if(process.argv[3])
+      {
+        let pos = 0;
+        if (process.argv[4])
+          pos = parseInt(process.argv[4]) - 1;
+
+        const filePath = exercisesFilePaths[process.argv[3]][pos]; // Only One file for now
+        if (filePath)
+          checkExerciseByName(filePath)
+        else
+          console.log(`Filepath does not exist for ${process.argv[3]}`);
+      }
+      else
+      {
+        console.log('please type the file name with path');
+        process.exit(-1);
+      }
+    }
+  }
+}
+
+main();
+
