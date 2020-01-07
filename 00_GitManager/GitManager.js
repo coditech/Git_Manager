a0b4737667d270d7fc7b135de6942e2a747a066e
Gaby Karam <gaby@codi.tech>
2020-01-07 13:39:09 +0200

    updating git repository for B05

--- 00_GitManager/GitManager.js
+++ 00_GitManager/GitManager.js
@@ -1,123 +1,129 @@
 var Git = require("nodegit");
-const fs = require('fs')
+const fs = require("fs");
 
 const RepositoryList = [
-  ["https://github.com/fadel100/Submissions", "Fadel"],
-  ["https://github.com/Haidara7/Submissions", "Haidara"],
-  ["https://github.com/HovigKevorkian/Submissions", "HovigKevorkian"],
-  ["https://github.com/jaafar21/Submissions", "Jaafar"],
-  ["https://github.com/Karimh438/Submissions", "Karim"],
-  ["https://github.com/liams98/Submissions", "William"],
-  ["https://github.com/Maissaash/Submissions", "Maissa"],
-  ["https://github.com/MARWAHAKMI/Submissions", "Marwa"],
-  ["https://github.com/Mayadihny/Submissions", "Maya"],
-  ["https://github.com/mohammadBaraka/Submissions", "M_Baraka"],
-  ["https://github.com/Mohammadzaher92/Submissions", "M_Zaher"],
-  ["https://github.com/myriam2008/Submissions", "Myriam"],
-  ["https://github.com/nooraldeenhmhm/Submissions", "Noor"],
-  ["https://github.com/omar-mhmd/Submissions", "Omar_M"],
-  ["https://github.com/RaedElNABOULSI/Submissions", "Raed"],
-  ["https://github.com/ridaFD/Submissions", "RidaFD"],
-  ["https://github.com/salamab/Submissions", "Salam_M"],
-  ["https://github.com/shirak96/Submissions", "Shirak"],
-  ["https://github.com/TheAwadianMan/Submissions", "OmarAwad"],
-  ["https://github.com/amanihaki/Submissions", "Amani"],
-  ["https://github.com/AshrafJurdi/Submissions", "Ashraf"],
-  ["https://github.com/BaselBarghouth/Submissions", "Basel"],
-  ["https://github.com/Basharfrancis/Submissions", "Bashar"],
-  ["https://github.com/BringSteam/Submissions", "Charbel"]
+  ["https://github.com/Abdallah2Kainassy/Submissions", "Abdallah_K"],
+  ["https://github.com/abdelmajeedjurdi/Submissions", "Abdelmajeed_J"],
+  ["https://github.com/AbdulkaderFreij/Submissions", "Abdulkader_F"],
+  ["https://github.com/ahmadmtaleb/Submissions", "Ahmad_T"],
+  ["https://github.com/Akram-m-Ahmad/Submissions", "Akram_A"],
+  ["https://github.com/ali-fawaz/Submissions", "Ali_F"],
+  ["https://github.com/basemEk/Submissions", "Bassem_E"],
+  ["https://github.com/Bassel17/Submissions", "Basel_k"],
+  ["https://github.com/BayanSwaid/Submissions", "Bayan_S"],
+  ["https://github.com/Behaajumaa/Submissions", "Behaa_J"],
+  ["https://github.com/Elmassri/Submissions", "Mahmoud_E"],
+  ["https://github.com/ghadban135/Submissions", "Ahmad_G"],
+  ["https://github.com/haddadanthony/Submissions", "Anthony_H"],
+  ["https://github.com/HasanAwad/Submissions", "Hasan_A"],
+  ["https://github.com/karimfarra/Submissions", "Karim_F"],
+  ["https://github.com/Malak-Kanaan/Submissions", "Malak_k"],
+  ["https://github.com/Marwa-Hodeib/Submissions", "Marwa_H"],
+  ["https://github.com/michelalachkar/Submissions", "Michel_A"],
+  ["https://github.com/mohamadmek/Submissions", "Mohamad_Me"],
+  ["https://github.com/nassirGH/Submissions", "Nassir_G"],
+  ["https://github.com/OussamaGhamian/Submissions", "Oussama_G"],
+  ["https://github.com/oussamaH95/Submissions", "Oussama_H"],
+  ["https://github.com/rania235/Submissions", "Rania_O"],
+  ["https://github.com/Rayan94-sudo/Submissions", "Rayan_J"],
+  ["https://github.com/RudzC/Submissions", "Rudolf_C"],
+  ["https://github.com/SandraNaim/Submissions", "Sandra_N"],
+  ["https://github.com/tinamechleb/Submissions", "Tina_M"],
+  ["https://github.com/WANASX/Submissions", "Anas_K"],
+  ["https://github.com/zeinabkassem27/Submissions", "Zenab_K"]
 ];
 
 const exercisesFilePaths = {
-  'cv-styling': ['Exercises/CV-Styling/public/style1/style.css', 'Exercises/CV-Styling/public/style2/style.css', 'Exercises/CV-Styling/public/style3/style.css'],
-}
+  "cv-styling": [
+    "Exercises/CV-Styling/public/style1/style.css",
+    "Exercises/CV-Styling/public/style2/style.css",
+    "Exercises/CV-Styling/public/style3/style.css"
+  ]
+};
 
 var rmDir = function(dir, rmSelf) {
   var files;
-  rmSelf = (rmSelf === undefined) ? true : rmSelf;
+  rmSelf = rmSelf === undefined ? true : rmSelf;
   dir = dir + "/";
-  try { files = fs.readdirSync(dir); } catch (e) { console.log("!Oops, directory not exist."); return; }
+  try {
+    files = fs.readdirSync(dir);
+  } catch (e) {
+    console.log("!Oops, directory not exist.");
+    return;
+  }
   if (files.length > 0) {
-      files.forEach(function(x, i) {
-          if (fs.statSync(dir + x).isDirectory()) {
-              rmDir(dir + x);
-          } else {
-              fs.unlinkSync(dir + x);
-          }
-      });
+    files.forEach(function(x, i) {
+      if (fs.statSync(dir + x).isDirectory()) {
+        rmDir(dir + x);
+      } else {
+        fs.unlinkSync(dir + x);
+      }
+    });
   }
   if (rmSelf) {
-      // check if user want to delete the directory ir just the files in this directory
-      fs.rmdirSync(dir);
+    // check if user want to delete the directory ir just the files in this directory
+    fs.rmdirSync(dir);
   }
-}
+};
 
 const cloneRepos = () => {
   RepositoryList.forEach(elem => {
-    if (fs.existsSync(`../${elem[1]}`))
-    {
+    if (fs.existsSync(`../${elem[1]}`)) {
       rmDir(`../${elem[1]}`);
       console.log(`Updating : ${elem[1]}...`);
     }
 
     Git.Clone(elem[0], "../" + elem[1]).then(function(repo) {
-      if (repo)
-        console.log(`Completed : ${elem[1]}`);
-    })
+      if (repo) console.log(`Completed : ${elem[1]}`);
+    });
   });
 };
 
-const checkExerciseByName = (path) =>
-{
-  const badPeeps = [], goodpeeps = [];
-  fs.readdir('../', function(err, items) {
+const checkExerciseByName = path => {
+  const badPeeps = [],
+    goodpeeps = [];
+  fs.readdir("../", function(err, items) {
     for (var i = 0; i < items.length; i++) {
       if (
         items[i] === ".git" ||
         items[i] === ".gitignore" ||
         items[i] === "00_GitManager" ||
         items[i] === "README.md"
-      ) continue;
+      )
+        continue;
 
-      const fullPath = '../' + items[i] + '/' + path;
+      const fullPath = "../" + items[i] + "/" + path;
       const studentName = items[i];
-      
-      if (!fs.existsSync(fullPath))
-        badPeeps.push(studentName);
-      else
-        goodpeeps.push(studentName);
+
+      if (!fs.existsSync(fullPath)) badPeeps.push(studentName);
+      else goodpeeps.push(studentName);
     }
-    console.log(`${JSON.stringify(badPeeps)} => Did not complete the exercise : ${path}`);
-    console.log(`${JSON.stringify(goodpeeps)} => Did complete the exercise : ${path}`)
+    console.log(
+      `${JSON.stringify(badPeeps)} => Did not complete the exercise : ${path}`
+    );
+    console.log(
+      `${JSON.stringify(goodpeeps)} => Did complete the exercise : ${path}`
+    );
   });
-}
+};
 
 const main = () => {
-  if (process.argv.length <= 2)
-    cloneRepos();
+  if (process.argv.length <= 2) cloneRepos();
   else {
-    if (process.argv[2] == 'check')
-    {
-      if(process.argv[3])
-      {
+    if (process.argv[2] == "check") {
+      if (process.argv[3]) {
         let pos = 0;
-        if (process.argv[4])
-          pos = parseInt(process.argv[4]) - 1;
+        if (process.argv[4]) pos = parseInt(process.argv[4]) - 1;
 
         const filePath = exercisesFilePaths[process.argv[3]][pos]; // Only One file for now
-        if (filePath)
-          checkExerciseByName(filePath)
-        else
-          console.log(`Filepath does not exist for ${process.argv[3]}`);
-      }
-      else
-      {
-        console.log('please type the file name with path');
+        if (filePath) checkExerciseByName(filePath);
+        else console.log(`Filepath does not exist for ${process.argv[3]}`);
+      } else {
+        console.log("please type the file name with path");
         process.exit(-1);
       }
     }
   }
-}
+};
 
 main();
-
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Git_Manager
+
 Simple Node Program to clone all students repositories. (Could be done in .SH)
 
+## Setup
 
-## Setup 
 ```sh
     git clone https://github.com/coditech/Git_Manager.git
     cd Git_Manager
@@ -13,13 +14,16 @@ Simple Node Program to clone all students repositories. (Could be done in .SH)
 ## Use
 
 #### Clone/Update Repository list.
+
 ```sh
     node ./GitManager.js
 ```
+
 #### Check Exercise/Challenges
+
 ```sh
     node ./GitManager.js check (exercise/challenge name) stepNumber
-    
+
     e.g cv-styling : node ./GitManager check cv-styling 2
     > return all student names that have not finished the exercise at step 2.
 ```
@@ -27,11 +31,13 @@ Simple Node Program to clone all students repositories. (Could be done in .SH)
 It should clone all submissions folder.
 
 ## Task List
-* [x] Implement Update Function.
-* [x] Implement Arguments.
-* [x] Implement Exercises/Challenges Checkup.
-* [ ] Implement List of Exercises/Challenges to verify.
-* [ ] Implement more arguments e.g : completed exercises etc..
-* [ ] Cleanup the Code.
-* [ ] Implement Code reading.
-* [ ] Implement Code Compare (with others students)
+
+- [x] Implement Update Function.
+- [x] Implement Arguments.
+- [x] Implement Exercises/Challenges Checkup.
+- [ ] Implement fetching repo list from API
+- [ ] Implement List of Exercises/Challenges to verify.
+- [ ] Implement more arguments e.g : completed exercises etc..
+- [ ] Cleanup the Code.
+- [ ] Implement Code reading.
+- [ ] Implement Code Compare (with others students)
